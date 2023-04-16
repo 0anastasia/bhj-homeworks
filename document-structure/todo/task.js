044227a113110f31787adefdebc18b6c804eed40
0anastasia <113232709+0anastasia@users.noreply.github.com>
2023-04-16 12:41:27 +0500

    Update task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,24 +3,28 @@ let tasksAdd = document.getElementById('tasks__add');
 let tasksList = document.getElementById('tasks__list');
 
 let addingTask = () => {
-    tasksList.innerHTML += `
-          <div class="task">
+    tasksList.insertAdjacentHTML('afterbegin', `
+        <div class="task">
             <div class="task__title">
-            ${taskInput.value}
+                ${taskInput.value}
             </div>
             <a href="#" class="task__remove">&times;</a>
-          </div>`;
-        taskInput.value = '';    
+        </div>`
+        )
+        taskInput.value = '';     
 }
-let taskRemove;
-let remove = () => {taskRemove = Array.from(tasksList.children);
-    if (taskRemove.length > 0) {
-    taskRemove.forEach(el => {el.lastElementChild.addEventListener('click', (event) => {
-            event.preventDefault();
-            event.target.closest('.task').remove()
-        })})
-    }
+
+let remove = () => {
+  let taskRemove = Array.from(tasksList.children);
+    taskRemove.forEach(el => {el.lastElementChild.addEventListener('click', e => {
+        e.preventDefault();
+        if(taskRemove.length > 0) {
+            e.target.closest('.task').remove()
+        }
+        
+    })})
 }
+
 let tasksAddButton = () => {tasksAdd.addEventListener('click', (event) => {
     if (taskInput.value.trim().length > 0) {
         event.preventDefault();
@@ -28,12 +32,5 @@ let tasksAddButton = () => {tasksAdd.addEventListener('click', (event) => {
     }
     remove();
   })
-}; tasksAddButton();
-let tasksAddEnter = () => {taskInput.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter' && taskInput.value.trim().length > 0) {
-          event.preventDefault();
-          addingTask();
-      } 
-      remove()    
-  })
-}; tasksAddEnter();
+}
+tasksAddButton();
